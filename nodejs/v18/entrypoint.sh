ab85b222aea3ce0a74a58c46d0ddb8f056282745
BasToTheMax <info@bastothemax.nl>
2023-09-26 16:44:08 +0200

    Try to fix weird bug

--- nodejs/v18/entrypoint.sh
+++ nodejs/v18/entrypoint.sh
@@ -2,24 +2,38 @@
 TZ=${TZ:-UTC}
 export TZ
 
+echo Hi
+
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+echo "Got internal IP - ${INTERNAL_IP}"
+
 # Switch to the container's working directory
 cd /home/container || exit 1
 
+echo Main dir exists
+
 # Print Node.js version
 # printf "\033[1m\033[33mcontainer@Meegie~ \033[0mnode -v\n"
-# node -v
+node -v
+echo Nodejs version
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
+echo Transformed variables
+
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
+
+echo Running...
+
 printf "\033[1m\033[33mcontainer@Meegie~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 exec env ${PARSED}
+
+echo Run done!
