a7095e95e96602a842ee71db3b2561bcab735153
BasToTheMax <info@bastothemax.nl>
2023-09-27 16:12:01 +0200

    Ads xD

--- nodejs/v18/entrypoint.sh
+++ nodejs/v18/entrypoint.sh
@@ -2,13 +2,12 @@
 TZ=${TZ:-UTC}
 export TZ
 
-echo Hi
+echo "Hi! :D"
+echo "> Egg by Meegie"
 
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
-
-echo "Got internal IP - ${INTERNAL_IP}"
 
 # Switch to the container's working directory
 cd /home/container || exit 1
